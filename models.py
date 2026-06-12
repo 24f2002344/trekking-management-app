4bf618aa0d1f9ef1f08028977dbb823501a91b26
dheerajk170884-stack <dheerajk170884@gmail.com>
2026-06-12 23:09:41 +0530

    database setup

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import userMixin
+from flask_login import UserMixin
 from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(userMixin,db.Model):
+class User(UserMixin,db.Model):
     __tablename__='users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(50),unique=True,nullable = False)
@@ -14,4 +14,34 @@ class User(userMixin,db.Model):
     phone = db.Column(db.String(15),nullable=False)
     role = db.Column(db.String(20),nullable = False,default='user')
     status = db.Column(db.String(20),nullable= False, default='active')
-    is_approved = db.Column(db.boolean,dafault = False)
+    is_approved = db.Column(db.Boolean,default = False)
+
+    bookings = db.relationship('Booking',backref='users',lazy=True)
+    assigned_treks = db.relationship('Trek',backref='users',lazy=True)
+
+class Trek(db.Model):
+    __tablename__ = 'treks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(150), nullable=False)
+    difficulty = db.Column(db.String(20), nullable=False)
+    duration = db.Column(db.String(50), nullable=False)
+    total_slots = db.Column(db.Integer, nullable=False)
+    available_slotes = db.Column(db.Integer,nullable=False)
+    assigned_staff_id = db.Column(db.Integer,db.ForeignKey('users.id'), nullable=True)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    bookings = db.relationship('Booking',backref='treks',lazy=True)
+
+class Booking(db.Model):
+    __tablename__='bookings'
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+    trek_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable=False)
+    booking_date = db.Column(db.DateTime,default=datetime.now)
+    status = db.Column(db.String(20),nullable = False,default='pending')
+    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
